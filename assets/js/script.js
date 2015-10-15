af172d337bf7bf5b31735257d17b69dfa4b4631a
lordmg <marianbergerot@gmail.com>
2015-10-15 18:39:16 +0200

    =vla

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
+  $('.vue').hide();
+  // Routeur
+  $('.link').on('click', function () {
+    console.log("ok");
+    var vue = $(this).attr("data-view");
     $('.vue').hide();
-    // Routeur
-    $('.link').on('click', function () {
-        console.log("ok");
-        var vue = $(this).attr("data-view");
-        $('.vue').hide();
-        $('.' + vue).show();
-    })
+    $('.' + vue).show();
+  })
+  var socket = io();
+  // socket.on('connect', function () { // TIP: you can avoid listening on `connect` and listen on events directly too!
+  //   socket.emit('ferret', 'tobi', function (data) {
+  //     console.log(data); // data will be 'woot'
+  //   });
+  // });
 });
